6cfc8a24d788c6cb05ba4bb2710a8a53f28a2f7f
17Atigogo_J <17Atigogo_J@bexleygs.co.uk>
2023-07-03 11:28:39 +0100

    Initial commit

---
+++ src/com/company/GUI.java
@@ -0,0 +1,25 @@
+package com.company;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GUI extends JFrame implements ActionListener {
+    JButton testButton;
+    public GUI(){
+        testButton = new JButton("January");
+        testButton.setBounds(10,10,150,50);
+        testButton.addActionListener(this);
+        getContentPane().add(testButton);
+        setLayout(null);
+        //pack();
+        setBounds(0,0,300,300);
+        setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getActionCommand().equals("January")){
+            System.out.println("hello");
+        }
+    }
+}
