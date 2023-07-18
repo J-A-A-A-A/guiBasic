1e3c2463fcbc37fd57e1120176befcf7cf7482c9
17Atigogo_J <17Atigogo_J@bexleygs.co.uk>
2023-07-18 09:19:26 +0100

    18/07/23

--- src/com/company/Calender.java
+++ src/com/company/Calender.java
@@ -3,13 +3,40 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
+import java.sql.Array;
 
 public class Calender extends JFrame implements ActionListener {
-    String[] months = {"January", "February", "March", "April", "May", "June", "July",
-                        "August", "September","October", "November", "December"};
-    int[] daysinMonths = {31,28,31,30,31,30,31,31,30,31,30,31};
-    int[] dayMonthstart = {1,4,4,7,2,5,7,3,6,1,4,6};
-    public Calender(){
+    String[] months = { "September","October", "November", "December","January", "February", "March", "April", "May", "June", "July", "August"};
+    int[] daysInMonths = {30,31,30,31,31,28,31,30,31,30,31,31};
+    int[] dayMonthStart = {6,1,4,6,1,4,4,7,2,5,7,3};
+    int year;
+
+    public void firstDayOfYear(int year){
+        int sum;
+        for (int y = 0; y<2;y++) {
+            year = year+y;
+            System.out.println(year);
+            int lastTwoDigits = (year % 100);
+            int quotient = lastTwoDigits / 4;
+            sum = lastTwoDigits + quotient;
+            if (year >= 2000) {
+                sum += 6;
+            } else {
+                sum += 5;
+            }
+            for(int x = 0; x<12;x++) {
+                if ((x<4 && y==0)||(x>3 && y==1)) {
+                    dayMonthStart[x] = (dayMonthStart[x] + sum) % 7;
+                    if (dayMonthStart[x] == 0) {
+                        dayMonthStart[x] = 7;
+                    }
+                }
+            }
+        }
+    }
+
+    public Calender(int year){
+        this.year = year;
         int width = 130;
         int height = 50;
         for (int y = 0; y < 2; y++) {
@@ -23,13 +50,15 @@ public class Calender extends JFrame implements ActionListener {
             setBounds(0,0,1000,250);
             setVisible(true);
         }
+        firstDayOfYear(year);
     }
+
     public Calender(int dayOfWeek,int daysInMonth){
         int week = 1;
         int width = 50;
         int height = 50;
-        for(int day = 1; day <= daysInMonth; day++){
-            JButton button = new JButton(Integer.toString(day));
+        for(int d = 1; d <= daysInMonth; d++){
+            JButton button = new JButton(Integer.toString(d));
             button.setBounds(0+((width+(width/5))*dayOfWeek),0+((height+(height/5))*week),width,height);
             button.addActionListener(this);
             getContentPane().add(button);
@@ -40,14 +69,14 @@ public class Calender extends JFrame implements ActionListener {
             dayOfWeek++;
         }
         setLayout(null);
-        setBounds(0,0,550,450);
+        setBounds(0,0,550,500);
         setVisible(true);
     }
     @Override
     public void actionPerformed(ActionEvent e) {
-        for(int x = 0; x<13;x++) {
+        for(int x = 0; x<12;x++) {
             if (e.getActionCommand().equals(months[x])) {
-                new Calender(dayMonthstart[x], daysinMonths[x]);
+                new Calender(dayMonthStart[x], daysInMonths[x]);
             }
         }
 
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,13 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	new Calender();
+	new Calender(2022);
     }
 }
+
+
+
